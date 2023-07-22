45a329e5f2141aa23018708baa81482b369cbace
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2023-07-22 22:53:28 +0700

    style: change layout forum

--- pages/forum.tsx
+++ pages/forum.tsx
@@ -1,10 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu, DropdownMenuContent, DropdownMenuLabel, DropdownMenuRadioGroup, DropdownMenuRadioItem, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu'
 import { Separator } from '@/components/ui/separator'
 import { getAuthUser } from '@/lib/utils'
-import { ChevronLeft, LogOut, Text, User, VenetianMask } from 'lucide-react'
+import { AlignJustify, Bug, ChevronDown, LogOut, TrendingUp, User, VenetianMask } from 'lucide-react'
 import { GetServerSideProps, NextPage } from 'next'
 import Link from 'next/link'
 import Head from 'next/head'
@@ -39,7 +38,7 @@ const Forum: NextPage<TProps> = ({ user }) => {
       method: "DELETE"
     })
 
-    if(req.ok) return router.push('/login')
+    if (req.ok) return router.push('/login')
   }
 
   return (
@@ -69,45 +68,37 @@ const Forum: NextPage<TProps> = ({ user }) => {
             lg:w-2/12 flex flex-col justify-between lg:sticky lg:top-0 
             py-4 lg:pr-2 lg:border-r-2 lg:border-b-2 lg:rounded-b-md 
             lg:border-secondary lg:px-0 lg:bg-transparent lg:translate-y-0
+            h-screen
 
-            fixed inset-0 z-10 px-4 bg-secondary ${openMenu ? 'translate-y-0' : '-translate-y-full'}
+            fixed inset-0 z-10 px-4 bg-secondary ${openMenu ? 'translate-y-0' : 'translate-y-full'}
             transition-all
           `}>
             <div>
-              <Button variant='outline' className='mb-2 w-full space-x-2 lg:hidden' onClick={() => setOpenMenu(false)}>
-                <ChevronLeft className='w-5 aspect-square' />
-                <span>Tutup Menu Ini</span>
-              </Button>
 
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button className='flex w-full items-center space-x-2'>
-                    <Text className='w-5 aspect-square' />
-                    <span>Diskusi {menu.toUpperCase()}</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-52">
-                  <DropdownMenuLabel>Kategori Diskusi</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuRadioGroup value={menu} onValueChange={setMenu}>
-                    <DropdownMenuRadioItem value="fyp">FYP</DropdownMenuRadioItem>
-                    <DropdownMenuRadioItem value="dev">Dari Developer</DropdownMenuRadioItem>
-                  </DropdownMenuRadioGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
+              <h2 className='text-lg font-bold'>Kategori Diskusi</h2>
+              <ul className='mt-2 space-y-2'>
+                <li onClick={() => setMenu('fyp')} className={`cursor-pointer flex items-center space-x-2 p-2 rounded-md border ${menu === 'fyp' ? 'bg-primary text-secondary' : 'bg-white hover:bg-secondary'}`}>
+                  <TrendingUp className='w-5 aspect-square' />
+                  <span className='text-md'>FYP</span>
+                </li>
+                <li onClick={() => setMenu('dev')} className={`cursor-pointer flex items-center space-x-2 p-2 rounded-md border ${menu === 'dev' ? 'bg-primary text-secondary' : 'bg-white hover:bg-secondary'}`}>
+                  <Bug className='w-5 aspect-square' />
+                  <span className='text-md'>Dari Developer</span>
+                </li>
+              </ul>
 
               <Separator className='my-4' />
 
               <h2 className='text-lg font-bold'>Pengaturan</h2>
               <ul className='mt-2 space-y-2'>
                 <li>
-                  <Link href='/pengaturan/akun' className='flex items-center space-x-2 p-2 hover:bg-secondary rounded-md border'>
+                  <Link href='/pengaturan/akun' className='flex items-center space-x-2 p-2 bg-white hover:bg-secondary rounded-md border'>
                     <User className='w-5 aspect-square' />
                     <span className='text-md'>Akun</span>
                   </Link>
                 </li>
                 <li>
-                  <Link href='/pengaturan/anonymous' className='flex items-center space-x-2 p-2 hover:bg-secondary rounded-md border'>
+                  <Link href='/pengaturan/anonymous' className='flex items-center space-x-2 p-2 bg-white hover:bg-secondary rounded-md border'>
                     <VenetianMask className='w-5 aspect-square' />
                     <span className='text-md'>Anonymous</span>
                   </Link>
@@ -118,27 +109,37 @@ const Forum: NextPage<TProps> = ({ user }) => {
 
             <div className='w-full'>
               <Separator className='my-4' />
-              <div className='flex items-center gap-4'>
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-                <div>
-                  <p className='font-bold'>{user.username}</p>
-                  <Link href={`/profil/${user.username}`} className='text-sm mt-0 hover:underline'>Lihat Profil</Link>
+
+              <div className='flex items-center justify-between gap-4'>
+                <div className='flex items-center gap-4'>
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className='font-bold'>{user.username}</p>
+                    <Link href={`/profil/${user.username}`} className='text-sm text-foreground/60 mt-0 hover:underline'>Lihat Profil</Link>
+                  </div>
                 </div>
+
+                <Button variant='destructive' className='space-x-2 w-max' onClick={logoutHandler}>
+                  <LogOut className='w-5 aspect-square' />
+                </Button>
               </div>
 
-              <Button variant='destructive' className='mt-4 space-x-2 w-full' onClick={logoutHandler}>
-                <LogOut className='w-5 aspect-square' />
-                <span>Keluar</span>
+              <Button variant='outline' className='my-4 w-full space-x-2 justify-between lg:hidden' onClick={() => setOpenMenu(false)}>
+                <span>Tutup Menu Ini</span>
+                <ChevronDown className='w-5 aspect-square' />
               </Button>
             </div>
           </aside>
 
           <main className='py-4 relative w-full lg:w-auto h-[2000px]'>
-            <div className='sticky top-4 pb-4 w-full lg:hidden'>
-              <Button className='w-full' onClick={() => setOpenMenu(true)}>Buka Menu</Button>
+            <div className='fixed inset-x-0 bottom-0 p-4 bg-secondary/50 border-t-2 border-secondary backdrop-blur-md w-full lg:hidden'>
+              <Button className='w-full flex items-center justify-between space-x-2' onClick={() => setOpenMenu(true)}>
+                <span>Buka Menu</span>
+                <AlignJustify className='w-5 aspect-square' />
+              </Button>
             </div>
             <p>Main Content Goes Here</p>
           </main>
